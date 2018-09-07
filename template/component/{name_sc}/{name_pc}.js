e9a2b9760589fb24f7459bc3c7b3ca4e1fb63fb8
ElRasco <luca.rasconi@gmail.com>
2018-09-07 14:37:33 +0200

    Update {name_pc}.js

--- template/component/{name_sc}/{name_pc}.js
+++ template/component/{name_sc}/{name_pc}.js
@@ -4,7 +4,7 @@ export default class {{name_pc}} extends Component
 {
     render(){
         return(
-            <div className="component-{{name_pc}}">
+            <div className="{{name_pc}}">
 
             </div>
         )
